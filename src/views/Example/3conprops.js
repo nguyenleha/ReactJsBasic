5b7caa9b730347df1bfc6e36d0fc5d4c98d0b3e7
Nguyễn Lê Hà <65054260+nguyenleha@users.noreply.github.com>
2022-07-11 16:26:58 +0700

    an hien

--- src/views/Example/3conprops.js
+++ src/views/Example/3conprops.js
@@ -1,26 +1,49 @@
 import React from "react";
 
 class Conprops extends React.Component {
-
+    state = {
+        hien: false,
+    }
+    anhien = (event) =>{
+        this.setState(
+            {
+                hien: !this.state.hien
+            }
+        )
+    }
     render() {
 
         const { Name, Lname, arr } = this.props;
+        const { hien } = this.state;
         console.log(">> check props", this.props)
         //dat ten giong ben formhtml
         return (
             <>
                 <div>Child Conprops: {Name} - {Lname}</div>
-                <div>
-                    {
-                        arr.map((item, index) => {
-                            return (
-                                <div key={item.id}>
-                                    {item.id} - {item.title} -{item.tien}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                {hien === false ?
+                    <div>
+                        <button onClick={(event) => this.anhien(event)}>hien</button>
+                    </div>
+                    :
+                    <>
+                        <div>
+                            {
+                                arr.map((item, index) => {
+                                    return (
+                                        <div key={item.id}>
+                                            {item.id} - {item.title} -{item.tien}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div><button onClick={(event) => this.anhien(event)}>an</button></div>
+                    </>
+                }
+
+
+
+
             </>
         )
     }
