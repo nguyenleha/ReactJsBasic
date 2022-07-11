b0ecce2d43ac31c38cb42b4577b5ed372c0560ed
Nguyễn Lê Hà <65054260+nguyenleha@users.noreply.github.com>
2022-07-11 13:00:00 +0700

    up

--- src/views/App.js
+++ src/views/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.scss';
 // import Statecoban from './Example/1statecoban';
-import Formhtmlcoban from './Example/2formhtmlcoban';
+// import Formhtmlcoban from './Example/2formhtmlcoban';
+import Formhtmlnangcao from './Example/3formhtmlnangcao';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        {/* <Statecoban /> */}
-        <Formhtmlcoban />
 
+        {/* <Statecoban /> */}
+        {/* <Formhtmlcoban /> */}
+        <Formhtmlnangcao />
       </header>
     </div>
   );
---
+++ src/views/Example/3formhtmlnangcao.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+class Formhtmlnangcao extends React.Component {
+    state = {
+        FirstName: "",
+        LastName: ""
+    }
+    thaydoiFirstName = (event) => {
+        this.setState({
+            FirstName: event.target.value
+        })
+    }
+    thaydoiLastName = (event) => {
+        this.setState({
+            LastName: event.target.value
+        })
+    }
+    bamdangnhap = (event) => {
+        event.preventDefault()
+        console.log('>>> check data: ',this.state)
+    }
+    render() {
+        const FirstName = this.state.FirstName
+        const LastName = this.state.LastName
+        return (
+            <>
+                <form >
+                    <label htmlFor="FName">{FirstName}</label>
+                    <br />
+                    <input
+                        type="text"
+                        value={FirstName}
+                        onChange={(event) => this.thaydoiFirstName(event)}
+                    />
+                    <br />
+                    <label>{LastName}</label>
+                    <br />
+                    <input
+                        type="text"
+                        value={LastName}
+                        onChange={(event) => this.thaydoiLastName(event)}
+                    />
+                    <br />
+                    <br />
+                    <input
+                        type="button"
+                        value="Submit"
+                        onClick={(event) => this.bamdangnhap(event)} />
+                </form>
+            </>
+        )
+    }
+}
+export default Formhtmlnangcao
