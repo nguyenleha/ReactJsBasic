e12c50b9c56726183eef74a8eae1506f62b1f26a
Nguyễn Lê Hà <65054260+nguyenleha@users.noreply.github.com>
2022-07-12 11:16:27 +0700

    scss

--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.scss';
-import Formhtmlnangcao4 from './Example/4cautrucform/4formhtmlnangcao';
+import Filescss5 from './Example/5addcss/5filescss';
 // import Statecoban from './Example/1-2-3coban/1statecoban';
 // import Formhtmlcoban from './Example/1-2-3coban/2formhtmlcoban';
 // import Formhtmlnangcao from './Example/1-2-3coban/3formhtmlnangcao';
+// import Formhtmlnangcao4 from './Example/4cautrucform/4formhtmlnangcao';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         {/* <Statecoban /> */}
         {/* <Formhtmlcoban /> */}
         {/* <Formhtmlnangcao /> */}
-        <Formhtmlnangcao4/>
+        {/* <Formhtmlnangcao4/> */}
+        <Filescss5/>
       </header>
     </div>
   );
---
+++ src/views/Example/5addcss/5filescss.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Filescss.scss";
+
+const Filescss5 = () =>{
+    return(
+        <div className="trangdau" style={{background: "blue"}}>
+            hello word
+        </div>
+    )
+}
+export default Filescss5
